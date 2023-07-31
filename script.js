276e9732bad41f764235fde139c3240eb66316d2
aniket mishra <aniketmishra300@gmail.com>
2023-07-31 22:55:55 +0530

    inheritance

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+// const parent = {
+//     surname : 'Mishra',
+//     display : function()
+//     {
+//         console.log("Hello .. Aniket.....")
+//     }
+// }
+
+// const child1 = Object.create(parent)
+// child1.name = "Aniket";
+// console.log(`${child1.name} ${child1.surname}`)
+
+
+
+
+// function Box(value) {
+//     this.value = value;
+//   }
+//   Box.prototype.getValue = function () {
+//     return this.value;
+//   };
+//   const box = new Box(1);
+  
+//   Box.prototype.getValue = function () {
+//     return this.value + 1;
+//   };
+//   box.getValue(); // 2
+  
+
+
+// Question 2nd
+
+// let arr = [a,b,c]
